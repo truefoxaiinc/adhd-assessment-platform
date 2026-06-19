cc8431a66fe55356593042f4ed64fcc5e7a2aee1
Mr-fuaaaadh <fuhadmk4@gmail.com>
2026-06-19 11:47:08 +0530

    add profile completion flag to user profile api

--- apps/users/schemas.py
+++ apps/users/schemas.py
@@ -5,10 +5,23 @@ from apps.users.models import Users
 """User Profile Schema"""
 class GetUserProfileDetailSchema(serializers.ModelSerializer):
     profile_image_url = serializers.SerializerMethodField()
+    is_completed = serializers.SerializerMethodField()
 
     class Meta:
         model = Users
-        fields = ['id', 'email', 'username', 'dob', 'gender', 'country', 'height', 'weight', 'profile_image', 'profile_image_url']
+        fields = [
+            'id',
+            'email',
+            'username',
+            'dob',
+            'gender',
+            'country',
+            'height',
+            'weight',
+            'profile_image',
+            'profile_image_url',
+            'is_completed',
+        ]
 
     def get_profile_image_url(self, instance):
         request = self.context.get('request')
@@ -18,6 +31,19 @@ class GetUserProfileDetailSchema(serializers.ModelSerializer):
         if request:
             return request.build_absolute_uri(image_url)
         return image_url
+
+    def get_is_completed(self, instance):
+        required_fields = [
+            instance.email,
+            instance.username,
+            instance.dob,
+            instance.gender,
+            instance.country,
+            instance.height,
+            instance.weight,
+            instance.profile_image,
+        ]
+        return all(bool(value) for value in required_fields)
 
     def to_representation(self, instance):
         datas = super().to_representation(instance)
