dcc24bf39c9fbf5e5ce8f67d91e4eb9e7bd711a5
Mr-fuaaaadh <fuhadmk4@gmail.com>
2026-07-22 10:36:20 +0530

    Select settings by environment

--- project_adhd/settings/__init__.py
+++ project_adhd/settings/__init__.py
@@ -1,1 +1,11 @@
-from project_adhd.settings.dev import *  # noqa: F401,F403
+import os
+
+
+DJANGO_ENV = os.environ.get('DJANGO_ENV', 'development').lower()
+
+if DJANGO_ENV in {'prod', 'production'}:
+    from project_adhd.settings.prod import *  # noqa: F401,F403
+elif DJANGO_ENV == 'test':
+    from project_adhd.settings.test import *  # noqa: F401,F403
+else:
+    from project_adhd.settings.dev import *  # noqa: F401,F403
--- project_adhd/settings/dev.py
+++ project_adhd/settings/dev.py
@@ -1,8 +1,8 @@
 from project_adhd.settings.base import *  # noqa: F401,F403
 
-DEBUG = get_env_value('DEBUG', True)
+DEBUG = False if IS_PRODUCTION else get_env_value('DEBUG', True)
 
-if DEBUG:
+if DEBUG and not IS_PRODUCTION:
     INSTALLED_APPS += ['debug_toolbar']
     MIDDLEWARE.insert(
         MIDDLEWARE.index('django.middleware.csrf.CsrfViewMiddleware'),
